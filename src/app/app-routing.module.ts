0eada422e1be56c6fa0659765241c4072bb31d6e
Maksym Peretiaha <m.peretiaha@newlinetechnologies.net>
2020-11-05 13:01:00 +0200

    clean useless instance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './Auth/AuthGuard';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
